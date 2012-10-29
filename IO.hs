48a63b6db8b2d8e84af6493621f4499fbfa6f8c7
London Haskell <londonhaskell@gmail.com>
2012-10-29 12:10:22 +0000

    Add some functions that interact with the outside world

--- IO.hs
+++ IO.hs
@@ -9,3 +9,17 @@ module Destruction where
 -- http://cdsmith.wordpress.com/2012/04/18/why-do-monads-matter/
 
 -- Going to jump straight to the Kleisli arrow version so no code yet!
+
+-- Define a couple of functions that interact with the outside world
+waitForInput :: String -> IO String
+waitForInput s = do { putStrLn s ; getLine }
+
+outputReverse :: String -> IO ()
+outputReverse s = putStrLn (reverse s)
+
+-- Define a main using these functions
+-- Using composition we want something like:
+-- main = ( outputReverse . waitForInput "Enter some test:")
+main = do
+           s <- waitForInput "Enter some test:"
+           outputReverse s
