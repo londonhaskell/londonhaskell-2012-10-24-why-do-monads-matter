30ab13886ea1b2acfc409d53eee380cd904b9a35
London Haskell <londonhaskell@gmail.com>
2012-10-29 14:51:41 +0000

    Change waitForInput and outputReverse functions to return IOActions

--- IO.hs
+++ IO.hs
@@ -11,18 +11,16 @@ module Destruction where
 -- Going to jump straight to the Kleisli arrow version so no code yet!
 
 -- Define a couple of functions that interact with the outside world
-waitForInput :: String -> IO String
-waitForInput s = do { putStrLn s ; getLine }
+waitForInput :: String -> IOAction String
+waitForInput s = Output s (Wait (\t -> Return t))
 
-outputReverse :: String -> IO ()
-outputReverse s = putStrLn (reverse s)
+outputReverse :: String -> IOAction ()
+outputReverse s = Output (reverse s) (Return ())
 
 -- Define a main using these functions
 -- Using composition we want something like:
 -- main = ( outputReverse . waitForInput "Enter some test:")
-main = do
-           s <- waitForInput "Enter some test:"
-           outputReverse s
+main = runIO ((outputReverse `composeIO` waitForInput) "Enter some test:")
 
 -- Each IOAction describes one type of action and the action
 -- to do next
@@ -30,11 +28,18 @@ data IOAction a = Output String (IOAction a) -- Write the string and then the ne
                 | Wait (String -> IOAction a) -- Get a string and pass it to the function that returns the next action
                 | Return a
 
+runIO :: IOAction a -> IO a
+runIO (Output s n) = do { putStrLn s ; runIO n }
+runIO (Wait f) = do { s <- getLine ; runIO (f s) }
+runIO (Return x) = do { return x }
+
 -- Kleisli composition
 composeIO :: (b -> IOAction c) -> (a -> IOAction b) -> (a -> IOAction c)
 composeIO f g x = case (g x) of
                    Output s n -> Output s (composeIO f (\_ -> n) ())
                    Wait h -> Wait ( \s -> composeIO f h s )
+                   Return r -> f r
 
 -- Kleisli identity
+idIO :: a -> IOAction a
 idIO x = Return x
