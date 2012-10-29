8e865a64880934e60883f07577769fc8fd705054
London Haskell <londonhaskell@gmail.com>
2012-10-29 10:38:25 +0000

    Change divBy function to indicate divide by zero error

--- Err.hs
+++ Err.hs
@@ -12,14 +12,16 @@ module Failure where
 -- Use division (by zero) as a function that
 -- could produce an error.
 -- (divBy 2) applied to 6 equals 3. 
-divBy :: Integer -> Integer -> Integer
-divBy x y = div y x
+divBy :: Integer -> Integer -> Err Integer
+divBy 0 y = Error
+divBy x y = OK (div y x)
 
 -- An example including function composition
-main = do
-            print ((divBy 2 . divBy 5) 100) -- (2310 / 5) / 2
-            print ((divBy 0 . divBy 7) 2310) -- (2310 / 7) / 0 => Divide By Zero Error!!!
-            print ((divBy 5 . divBy 11) 2310) -- (2310 / 11) / 5
+-- Comment out because of error with composition since types don't line up now
+-- main = do
+--             print ((divBy 2 . divBy 5) 100) -- (2310 / 5) / 2
+--             print ((divBy 0 . divBy 7) 2310) -- (2310 / 7) / 0 => Divide By Zero Error!!!
+--             print ((divBy 5 . divBy 11) 2310) -- (2310 / 11) / 5
 
 -- Add an Err data-type that can record Success / Failure
 data Err a = OK a
